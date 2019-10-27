43e36a8dbbb0738a6b171c413d91e266968ea699
sbang002 <sbang002@github.com>
2019-10-27 16:56:52 -0700

    Updates

---
+++ precache-manifest.78da36aab5005773233abddb286cf115.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf9b0c5329006ff66996",
+    "url": "/sehoonbang/static/js/runtime~main.d2fb054c.js"
+  },
+  {
+    "revision": "b1d26821d44ea066199d",
+    "url": "/sehoonbang/static/js/main.730fc2a7.chunk.js"
+  },
+  {
+    "revision": "fe60e8fdfee2abf24334",
+    "url": "/sehoonbang/static/js/2.c3ef4538.chunk.js"
+  },
+  {
+    "revision": "b1d26821d44ea066199d",
+    "url": "/sehoonbang/static/css/main.766a7ce3.chunk.css"
+  },
+  {
+    "revision": "624c7908c78f00d5b7ae1b3d718ebf95",
+    "url": "/sehoonbang/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sbang002.github.io/precache-manifest.0a688aca2d2369e43007a030e12d25e5.js"
+  "/sehoonbang/precache-manifest.78da36aab5005773233abddb286cf115.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/sbang002.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/sehoonbang/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
