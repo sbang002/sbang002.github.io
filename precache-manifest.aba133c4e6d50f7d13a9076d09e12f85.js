23d2487c33a1447749c0a21c48d8ac506a83ef21
sbang002 <sbang002@github.com>
2019-10-27 16:51:59 -0700

    Updates

---
+++ precache-manifest.aba133c4e6d50f7d13a9076d09e12f85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dd7d56f38ca698165a21",
+    "url": "/static/js/main.5f20cdfa.chunk.js"
+  },
+  {
+    "revision": "fe60e8fdfee2abf24334",
+    "url": "/static/js/2.c3ef4538.chunk.js"
+  },
+  {
+    "revision": "dd7d56f38ca698165a21",
+    "url": "/static/css/main.766a7ce3.chunk.css"
+  },
+  {
+    "revision": "fa3f8880a62e6a2cc90a1828a9c3245a",
+    "url": "/index.html"
+  }
+];
